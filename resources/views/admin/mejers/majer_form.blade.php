96cedfc276312c88f0162348a5e08ca71295c702
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-04-05 03:43:28 +0300

    Feat : some of the inset view

---
+++ resources/views/admin/mejers/majer_form.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin_master_layout');
+@section('content')
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="card mb-4">
+            <h5 class="card-header">Add new majer</h5>
+            <form class="card-body" method="POST" action="/admin/categories/add" enctype="multipart/form-data">
+
+                <div class="row g-3">
+                    <div class="col-md-9">
+                        <label class="form-label" for="multicol-username">title</label>
+                        <input type="text" id="multicol-username" name='title' class="form-control"
+                            placeholder="Software development" />
+                    </div>
+                    <div class="col-md-3 mt-5">
+                        <label class="switch">
+                            <input type="checkbox" class="switch-input is-valid" checked />
+                            <span class="switch-toggle-slider">
+                                <span class="switch-on"></span>
+                                <span class="switch-off"></span>
+                            </span>
+                            <span class="switch-label">Valid</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="pt-4">
+                    <button type="submit" class="btn btn-primary me-sm-3 me-1">Submit</button>
+                    <button type="reset" class="btn btn-label-secondary">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/serives/services_form.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin_master_layout');
+@section('content')
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="card mb-4">
+            <h5 class="card-header">Add new service</h5>
+            <form class="card-body" method="POST" action="/admin/categories/add" enctype="multipart/form-data">
+                <div class="input-group">
+                    <input type="file" class="form-control" name='image' id="inputGroupFile02">
+                    <label class="input-group-text" for="inputGroupFile02">Upload Image </label>
+                </div>
+                <div class="row g-3">
+                    <div class="col-md-9">
+                        <label class="form-label" for="multicol-username">title</label>
+                        <input type="text" id="multicol-username" name='name' class="form-control"
+                            placeholder="Software development" />
+                    </div>
+                    <div class="col-md-3 mt-5">
+                        <label class="switch">
+                            <input type="checkbox" class="switch-input is-valid" checked />
+                            <span class="switch-toggle-slider">
+                                <span class="switch-on"></span>
+                                <span class="switch-off"></span>
+                            </span>
+                            <span class="switch-label">Valid</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <label class="form-label" for="multicol-username">About the compeny</label>
+                    <textarea type="text" id="multicol-username" name='des' class="form-control" placeholder="We are ..."></textarea>
+                </div>
+
+                <div class="pt-4">
+                    <button type="submit" class="btn btn-primary me-sm-3 me-1">Submit</button>
+                    <button type="reset" class="btn btn-label-secondary">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
